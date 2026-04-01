02399744a6228602c13a1f8a803e54f93343025a
RammonBarbosa <rammonbarbosac@gmail.com>
2026-04-01 14:49:17 -0300

    "env"

--- app/extract.py
+++ app/extract.py
@@ -1,13 +1,12 @@
 import requests
-
+import os
 def extract_data():
     # Substitua pelas suas chaves do Trello (conseguidas no Trello Power-Up Admin)
-    API_KEY = 'SUA_KEY_AQUI'
-    TOKEN = 'SEU_TOKEN_AQUI'
-    BOARD_ID = 'ID_DO_SEU_QUADRO' # Aquele código que aparece na URL do seu quadro
+    API_KEY = os.getenv('TRELLO_API_KEY')
+    TOKEN = os.getenv('TRELLO_TOKEN')
+    BOARD_ID = 'GstiuzaC' # Aquele código que aparece na URL do seu quadro
     
     url = f"https://api.trello.com/1/boards/{BOARD_ID}/cards?key={API_KEY}&token={TOKEN}"
-    
     response = requests.get(url)
     if response.status_code == 200:
         print("Extração concluída com sucesso!")
