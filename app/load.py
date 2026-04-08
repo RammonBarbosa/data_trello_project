e9f666ffffac0eaae00ad82536cd080abe0ad13c
RammonBarbosa <rammonbarbosac@gmail.com>
2026-04-08 13:34:35 -0300

    up supabase

--- app/load.py
+++ app/load.py
@@ -1,13 +1,30 @@
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def save_to_postgres(df):
+    # Puxando o nome exato que você colocou no .env
+    database_url = os.getenv("DIRECT_URL")
     
-#Função para conectar ao banco e salvar os dados do Trello.
-#Criamos a conexão (A Ponte)
-#Importante: usamos 'db' porque é o nome do serviço no docker-compose
-    engine = create_engine("postgresql://user:password@db:5432/trello_db")
-#Enviamos os dados (O Transporte)
-    print("Enviando dados para o PostgreSQL...")
-    df.to_sql("cards", engine, if_exists="replace", index=False)
+    if not database_url:
+        print("❌ Erro: DIRECT_URL não encontrada no arquivo .env")
+        return
+
+    # Criando o engine com 'pool_pre_ping' (verifica se a conexão está viva)
+    # e 'isolation_level' para resolver o erro de transação inválida
+    engine = create_engine(
+        database_url,
+        pool_pre_ping=True,
+        isolation_level="AUTOCOMMIT"
+    )
+
+    print("Conectando via DIRECT_URL ao Supabase...")
     
-    print("Dados salvos com sucesso na tabela 'cards'!")
+    try:
+        # Enviando os dados
+        df.to_sql("cards", engine, if_exists="replace", index=False)
+        print("✅ SUCESSO! Dados salvos no Supabase via Direct URL.")
+    except Exception as e:
+        print(f"❌ Erro na carga: {e}")
